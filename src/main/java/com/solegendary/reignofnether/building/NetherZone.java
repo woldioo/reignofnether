c191882e84f18d1a71d9128121b4b22d66e21b1a
Colin Vaughn <Colinvaughn01@gmail.com>
2024-11-06 00:49:37 -0500

    undo netherzone changes

--- src/main/java/com/solegendary/reignofnether/building/NetherZone.java
+++ src/main/java/com/solegendary/reignofnether/building/NetherZone.java
@@ -18,19 +18,17 @@ import java.util.logging.Logger;
 
 public class NetherZone {
 
-    private static final Logger LOGGER = Logger.getLogger(NetherZone.class.getName());
-
-    private static final int MAX_TICKS = 5;
-    private static final int MAX_CONVERTS_AFTER_CONSTANT_RANGE = 60;
+    private final int MAX_TICKS = 5;
+    private final int MAX_CONVERTS_AFTER_CONSTANT_RANGE = 60; // to prevent continuously processing
 
     private final BlockPos origin;
     private final Random random = new Random();
 
-    private boolean isRestoring = false;
+    private boolean isRestoring = false; // if false, start converting Nether back into Overworld blocks
     private final double maxRange;
     private double range;
     private int ticksLeft = MAX_TICKS;
-    private int convertsAfterConstantRange = 0;
+    private int convertsAfterConstantRange = 0; // after reaching max or min range, keep converting for a few more ticks
 
     public BlockPos getOrigin() { return origin; }
     public double getRange() { return range; }
@@ -63,119 +61,109 @@ public class NetherZone {
     public void startRestoring() {
         convertsAfterConstantRange = 0;
         isRestoring = true;
-        LOGGER.info("Started restoring blocks.");
     }
 
     public boolean isDone() {
-        boolean done = isRestoring && convertsAfterConstantRange >= MAX_CONVERTS_AFTER_CONSTANT_RANGE;
-        if (done) {
-            LOGGER.info("NetherZone restoration or conversion is complete.");
-        }
-        return done;
+        return isRestoring && convertsAfterConstantRange >= MAX_CONVERTS_AFTER_CONSTANT_RANGE;
     }
 
     public void tick(ServerLevel level) {
         if (!level.isClientSide()) {
-            if (--ticksLeft <= 0 && convertsAfterConstantRange < MAX_CONVERTS_AFTER_CONSTANT_RANGE) {
+            ticksLeft -= 1;
+            if (ticksLeft <= 0 && convertsAfterConstantRange < MAX_CONVERTS_AFTER_CONSTANT_RANGE) {
                 if (!isRestoring) {
                     netherConvertTick(level);
-                    if (range < maxRange) {
-                        range += 0.1;
-                    } else {
-                        convertsAfterConstantRange++;
-                    }
-                } else {
+                    if (range < maxRange)
+                        range += 0.1f;
+                    else
+                        convertsAfterConstantRange += 1;
+                }
+                else {
                     overworldRestoreTick(level);
-                    if (range > 0) {
-                        range -= 0.05;
-                    } else {
-                        convertsAfterConstantRange++;
-                    }
+                    if (range > 0)
+                        range -= 0.05f;
+                    else
+                        convertsAfterConstantRange += 1;
                 }
                 ticksLeft = MAX_TICKS;
             }
         }
     }
 
+    // randomly convert nether blocks into overworld blocks at decreasing ranges
     private void overworldRestoreTick(ServerLevel level) {
         double restoreRange = range + 5;
-        Set<BlockPos> bps = generateBlockPositions(restoreRange);
 
-        int conversions = 0;
-        for (BlockPos bp : bps) {
-            if (bp.distSqr(origin) < range * range) continue;
+        ArrayList<BlockPos> bps = new ArrayList<>();
+        for (double x = -restoreRange; x < restoreRange; x++)
+            for (double y = -restoreRange/2; y < restoreRange/2; y++)
+                for (double z = -restoreRange; z < restoreRange; z++)
+                    bps.add(origin.offset(x, y, z));
 
-            if (random.nextDouble() <= 0.10) {
-                for (NetherZone ncz : BuildingServerEvents.netherZones) {
-                    if (!ncz.isRestoring && bp.distSqr(ncz.origin) < ncz.maxRange * ncz.maxRange) {
-                        continue;
-                    }
-                }
-
-                BlockState bs = NetherBlocks.getOverworldBlock(level, bp);
-                BlockState bsPlant = NetherBlocks.getOverworldPlantBlock(level, bp.above(), true);
-                if (bs != null && !BuildingUtils.isPosPartOfAnyBuilding(level.isClientSide(), bp, true, (int) (maxRange * 2))) {
-                    level.setBlockAndUpdate(bp, bs);
-                    if (bsPlant != null) {
-                        level.setBlockAndUpdate(bp.above(), bsPlant);
-                    }
-                    conversions++;
-                }
-            }
-        }
-        LOGGER.info("Overworld restore tick converted " + conversions + " blocks.");
-    }
-
-    private void netherConvertTick(ServerLevel level) {
-        Set<BlockPos> bps = generateBlockPositions(range);
-        double rangeSqr = range * range;
-        double maxRangeSqr = maxRange * maxRange;
-
-        int conversions = 0;
+        outerloop:
         for (BlockPos bp : bps) {
             double distSqr = bp.distSqr(origin);
-            if (distSqr > rangeSqr) continue;
+            double rangeSqr = range * range;
+            double rangeMaxSqr = maxRange * maxRange;
+            if (distSqr < rangeSqr)
+                continue;
 
-            double chance = (1 - (distSqr / maxRangeSqr)) / 10;
-            if (isAdjacentToObsidianWater(level, bp)) chance = 1.0;
+            double chance = 0.10f;
+            if (random.nextDouble() > chance)
+                continue;
 
-            if (random.nextDouble() <= chance) {
-                BlockState bs = NetherBlocks.getNetherBlock(level, bp);
-                BlockState bsPlant = NetherBlocks.getNetherPlantBlock(level, bp.above());
-                if (bs != null && !BuildingUtils.isPosPartOfAnyBuilding(level.isClientSide(), bp, true, (int) (maxRange * 2))) {
-                    level.setBlockAndUpdate(bp, bs);
-                    if (bsPlant != null) {
-                        level.setBlockAndUpdate(bp.above(), bsPlant);
-                    }
-                    conversions++;
-                }
+            for (NetherZone ncz : BuildingServerEvents.netherZones)
+                if (!ncz.isRestoring && bp.distSqr(ncz.origin) < ncz.maxRange * ncz.maxRange)
+                    continue outerloop;
+
+            BlockState bs = NetherBlocks.getOverworldBlock(level, bp);
+            BlockState bsPlant = NetherBlocks.getOverworldPlantBlock(level, bp.above(), true);
+            if (bs != null && !BuildingUtils.isPosPartOfAnyBuilding(level.isClientSide(), bp, true, (int) (maxRange * 2))) {
+                level.setBlockAndUpdate(bp, bs);
+                if (bsPlant != null)
+                    level.setBlockAndUpdate(bp.above(), bsPlant);
             }
         }
-        LOGGER.info("Nether convert tick converted " + conversions + " blocks.");
     }
 
-    private Set<BlockPos> generateBlockPositions(double range) {
-        Set<BlockPos> bps = new HashSet<>();
-        for (int x = (int) -range; x <= range; x++) {
-            for (int y = (int) -range / 2; y <= range / 2; y++) {
-                for (int z = (int) -range; z <= range; z++) {
+    // randomly convert overworld blocks into nether blocks at increasing ranges
+    private void netherConvertTick(ServerLevel level) {
+        ArrayList<BlockPos> bps = new ArrayList<>();
+        for (double x = -range; x < range; x++)
+            for (double y = -range/2; y < range/2; y++)
+                for (double z = -range; z < range; z++)
                     bps.add(origin.offset(x, y, z));
-                }
+
+        for (BlockPos bp : bps) {
+            double distSqr = bp.distSqr(origin);
+            double rangeSqr = range * range;
+            double rangeMaxSqr = maxRange * maxRange;
+            if (distSqr > rangeSqr)
+                continue;
+
+            // give a higher chance to convert blocks at closer distance
+            double chance = (1 - (distSqr / rangeMaxSqr)) / 10;
+
+            if (level.getBlockState(bp).getBlock() == Blocks.WATER ||
+                    level.getBlockState(bp).getBlock() == Blocks.BUBBLE_COLUMN) {
+                int adjObs = 0;
+                if (level.getBlockState(bp.north()).getBlock() == Blocks.OBSIDIAN) adjObs += 1;
+                if (level.getBlockState(bp.south()).getBlock() == Blocks.OBSIDIAN) adjObs += 1;
+                if (level.getBlockState(bp.east()).getBlock() == Blocks.OBSIDIAN) adjObs += 1;
+                if (level.getBlockState(bp.west()).getBlock() == Blocks.OBSIDIAN) adjObs += 1;
+                if (adjObs >= 3)
+                    chance = 1.0f;
+            }
+            if (random.nextDouble() > chance)
+                continue;
+
+            BlockState bs = NetherBlocks.getNetherBlock(level, bp);
+            BlockState bsPlant = NetherBlocks.getNetherPlantBlock(level, bp.above());
+            if (bs != null && !BuildingUtils.isPosPartOfAnyBuilding(level.isClientSide(), bp, true, (int) (maxRange * 2))) {
+                level.setBlockAndUpdate(bp, bs);
+                if (bsPlant != null)
+                    level.setBlockAndUpdate(bp.above(), bsPlant);
             }
         }
-        return bps;
-    }
-
-    private boolean isAdjacentToObsidianWater(ServerLevel level, BlockPos bp) {
-        if (level.getBlockState(bp).getBlock() == Blocks.WATER ||
-                level.getBlockState(bp).getBlock() == Blocks.BUBBLE_COLUMN) {
-            int adjObs = 0;
-            if (level.getBlockState(bp.north()).getBlock() == Blocks.OBSIDIAN) adjObs++;
-            if (level.getBlockState(bp.south()).getBlock() == Blocks.OBSIDIAN) adjObs++;
-            if (level.getBlockState(bp.east()).getBlock() == Blocks.OBSIDIAN) adjObs++;
-            if (level.getBlockState(bp.west()).getBlock() == Blocks.OBSIDIAN) adjObs++;
-            return adjObs >= 3;
-        }
-        return false;
     }
 }
